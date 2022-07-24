c950345af07bd318e95225f77830bd34a95485e4
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 13:15:19 +0430

    adding error-middleware

---
+++ modules/routes/middlewares/error-middleware.js
@@ -0,0 +1,7 @@
+export default  async (error , req , res ,next )=>{
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data}) 
+
+}
