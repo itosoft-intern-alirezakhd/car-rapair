e53a1cdd3ec9207d96c2f81a2760cf92718f864c
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 14:00:05 +0430

    refactor api-v1.js

--- modules/routes/api/public/api-v1.js
+++ modules/routes/api/public/api-v1.js
@@ -1,41 +1,73 @@
-// const express = require('express');
-import express from "express";
+import express from "express"
+import config from '../../../../config';
+const {public : publicController} = config.path.controllersApi.v1
+const {middleware } = config.path
+const {controller} = config.path
 
+//import Controller
+import * as roleController from `${controller}/rolesController`
+import * as userController from `${controller}/usersController`
+
+//auth 
+import  registerController from `${publicController}/auth/registerController`
+import whoController from `${publicController}/auth/whoController`
+import * as loginController from `${publicController}/auth/loginController`
+//car
+import * as carController from `${publicController}/car/carController`
+//role
+import * as roleController from `${publicController}/role/roleController`
+//user
+import * as userController from `${publicController}/user/userController`
+ 
+//import middleware
+import allowLoggedIn from `${middleware}/allow-loggedIn-middleware`
+import grantAccess from `${middleware}}/grant-access-middleware`
+
+//Router
 const router = express.Router();
 
-// const roleController = require('./roles');
-// import './roles.js'
-import * as Role from '../../roles.js'
-import * as User from '../../users.js'
-import * as Car from '../../cars.js'
-// const userController = require('./users');
-// const fileUpload = require('express-fileupload');
+//auth 
+const authRouter = express.Router();
+authRouter.post('/signup', registerController.signUp.bind(registerController) );
+authRouter.post('/login', loginController.login.bind(loginController));
+authRouter.post('/loginWithOTP', loginController.loginWithOTP.bind(loginController));
+authRouter.post('/verifyOTP', loginController.verifyOTP.bind(loginController));
+authRouter.get('/whoAmI', allowLoggedIn, whoController.whoAmI.bind(whoController));
+router.use('/auth' , authRouter);
 
 
-router.post('/auth/signup', User.signUp);
-router.post('/auth/login', User.login);
-router.post('/auth/loginWithOTP', User.loginWithOTP);
-router.post('/auth/verifyOTP', User.verifyOTP);
-router.get('/auth/whoAmI', User.allowIfLoggedIn, User.whoAmI);
-router.get('/profile', User.allowIfLoggedIn, User.grantAccess('readAny', 'profile'), User.profile);
-router.post('/users/getAll', User.allowIfLoggedIn, User.grantAccess('readAny', 'user'), User.getUsers);
-router.post('/users/create', User.allowIfLoggedIn, User.grantAccess('createAny', 'user'), User.signUp);
-router.put('/users/update', User.allowIfLoggedIn, User.grantAccess('updateAny', 'user'), User.updateUser);
-router.delete('/users/delete', User.allowIfLoggedIn, User.grantAccess('deleteAny', 'user'), User.deleteUser);
-
-router.post('/roles/create', User.allowIfLoggedIn, User.grantAccess('createAny', 'role'), Role.addRole);
-router.post('/roles/getAll', User.allowIfLoggedIn, User.grantAccess('readAny', 'role'), Role.getRoles);
-router.put('/roles/update', User.allowIfLoggedIn, User.grantAccess('updateAny', 'role'), Role.updateRole);
-router.delete('/roles/delete', User.allowIfLoggedIn, User.grantAccess('deleteAny', 'role'), Role.deleteRole);
-router.get('/roles/distinct', User.allowIfLoggedIn, User.grantAccess('readAny', 'role'), Role.distinct);
-
-router.post('/cars/create', User.allowIfLoggedIn, Car.addCar)
-router.post('/cars/getAll', User.allowIfLoggedIn, Car.getAll)
-router.put('/cars/update', User.allowIfLoggedIn, Car.updateCar)
-router.delete('/cars/delete', User.allowIfLoggedIn, Car.deleteCar)
-// Authentication
-// router.get('/auth/v1', userController.authentication);
+//profile 
+const profileRouter = express.Router();
+profileRouter.get('/', allowLoggedIn, grantAccess('readAny', 'profile'), User.profile);
+router.use('/profile' , profileRouter);
 
 
-// module.exports = router;
+
+//User
+const userRouter = express.Router();
+userRouter.post('/getAll', allowLoggedIn, grantAccess('readAny', 'user'), userController.getUsers.bind(userController));
+userRouter.post('/create', allowLoggedIn, grantAccess('createAny', 'user'), userController.signUp.bind(userController));
+userRouter.put('/update', allowLoggedIn, grantAccess('updateAny', 'user'), userController.updateUser.bind(userController));
+userRouter.delete('/delete', allowLoggedIn, grantAccess('deleteAny', 'user'), userController.dele.bind(userController));
+router.use('/users' , userRouter);
+
+
+//Role
+const roleRouter = express.Router();
+roleRouter.post('/create', allowLoggedIn, grantAccess('createAny', 'role'), roleController.addRole.bind(roleController));
+roleRouter.post('/getAll', allowLoggedIn, grantAccess('readAny', 'role'), roleController.getRoles.bind(roleController));
+roleRouter.put('/update', allowLoggedIn, grantAccess('updateAny', 'role'), roleController.updateRole.bind(roleController));
+roleRouter.delete('/delete', allowLoggedIn, grantAccess('deleteAny', 'role'), roleController.deleteRole.bind(roleController));
+roleRouter.get('/distinct', allowLoggedIn, grantAccess('readAny', 'role'), roleController.distinct.bind(roleController));
+router.use('/roles' , roleRouter);
+
+
+//Car
+const carRouter = express.Router();
+carRouter.post('/cars/create', allowLoggedIn, carController.addCar.bind(carController))
+carRouter.post('/cars/getAll', allowLoggedIn, carController.getAll.bind(carController))
+carRouter.put('/cars/update', allowLoggedIn, carController.updateCar.bind(carController))
+carRouter.delete('/cars/delete', allowLoggedIn, carController.deleteCar.bind(carController))
+router.use('/cars' , carRouter);
+
 export default router
