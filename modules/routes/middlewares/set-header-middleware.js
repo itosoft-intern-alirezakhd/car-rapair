21cfdf513e933d06c7312c9bcf51f746d5dbb47c
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 12:43:54 +0430

    adding set-header-middleware to attach request

---
+++ modules/routes/middlewares/set-header-middleware.js
@@ -0,0 +1,7 @@
+export default (req, res, next) => {
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type Authorization');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Authorization, Accept');
+	next();
+};
