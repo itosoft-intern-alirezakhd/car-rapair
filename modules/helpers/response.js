8b610ed64f603defb7ceb208ecec58acc2383971
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 13:27:48 +0430

    adding response helper

---
+++ modules/helpers/response.js
@@ -0,0 +1,16 @@
+export default response = async (res, msg, logcode, status, data = null, msgStatus = 100, field = null) => {
+    let success = false;
+    if (status === 200 || status === 201) success = true;
+    return res.status(status).json({
+      message: {
+        field: field,
+        message: msg,
+        logcode: logcode,
+        status: msgStatus,
+      },
+      status,
+      success,
+      data: data || null,
+      v: releasesV,
+    });
+  };
