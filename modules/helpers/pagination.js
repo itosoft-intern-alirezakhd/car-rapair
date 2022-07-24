c4b5b406fb5b5d2ceda4e9b72b37c8ba5017f765
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 13:28:00 +0430

    adding pagination helper

---
+++ modules/helpers/pagination.js
@@ -0,0 +1,13 @@
+
+
+const Pagination = body => {
+    let page;
+    let size;
+    if (body.page < 1 || !body.page) page = 0;
+    else page = body.page - 1;
+    if (body.size < 1 || !body.size) size = 1;
+    else size = body.size;
+    return {size, page}
+};
+
+export default Pagination
