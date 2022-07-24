67ac691c08aeaa85748b58e97a6d364d667396da
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 13:26:57 +0430

    adding axios helper

---
+++ modules/helpers/axios.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const axios = async (method ,  url , header , data) =>{
+    try {
+        const Data = qs.stringify(data);
+        const axiosConfig = {
+          method: method,
+          url: url,
+          headers: header,
+          data: Data,
+        };
+        const response = await axios(axiosConfig);
+        return response;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+
+
+}
+
+
+export default axios
