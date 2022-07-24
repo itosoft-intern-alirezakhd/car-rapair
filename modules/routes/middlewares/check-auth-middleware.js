38a0dd19cc436a5261cb0509104ff219fe36e3e6
alireza <alirezakoohzad2705@gmail.com>
2022-07-24 12:44:23 +0430

    adding check-auth-middleware in middleware folder

---
+++ modules/routes/middlewares/check-auth-middleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+import User from './modules/models/userModel.js'
+
+
+export default async (req, res, next) => {
+	if (req.headers["authorization"]) {
+		const re = new RegExp('Bearer (.*)');
+		let accessToken = req.headers["authorization"].match(re)
+		if (accessToken === null) return res.status(401).json({error: 'Please enter a valid format for token'})
+		jwt.verify(accessToken[1], process.env.JWT_SECRET, async (err, decoded) => {
+			if (err && err.message) return res.status(401).json({error: `${err.message}, please login to obtain a new one`});
+			// const user = await User.findById(decoded.userId);
+			// if (user.accessToken !== accessToken) return res.status(401).json({error: `jwt expired, please login to obtain a new one`});
+			else res.locals.loggedInUser = await User.findById(decoded.userId);
+			next();
+		});
+	} else {
+		next();
+	}
+}
