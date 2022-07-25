78364c59158cab861138a4c85fc475c47d5b4e71
alireza <alirezakoohzad2705@gmail.com>
2022-07-25 12:24:29 +0430

    debugging register route

--- modules/controllers/api/v1/publicController/auth/registerController.js
+++ modules/controllers/api/v1/publicController/auth/registerController.js
@@ -1,63 +1,103 @@
 import emailRegex from 'email-regex'
 import InitializeController from './initializeController.js'; 
+import jwt from 'jsonwebtoken'
+import otpGenerator from 'otp-generator';
+import Otp from '../../../../../models/otp-model.js';
+import bcrypt from 'bcrypt'
 
 
 export default new(class RegisterController extends InitializeController {
-
+    //save user => create otp => send otp sms => verify otp
     async signUp  (req, res, next)  {
         try {
-            jwt.verify(req.body.registerToken, process.env.JWT_SECRET, async (err, decoded) => {
-                if (err && err.message)
-                    return this.abort(res , 401 , logcode ,`${err.message}, please login to obtain a new one` )
-                const otp = await this.model.Otp.findById(decoded.userId)
-                if (!otp) return this.abort(res , 400 , logcode ,"You use an Expired OTP!" ) 
-                const {name,username,email,password,contact} = req.body;
-                const role = "basic"
-                if (!emailRegex({
+            const {name,username,email,password,contact,mobile} = req.body;
+            const role = "basic"
+            if (!emailRegex({
                         exact: true
-                    }).test(email)) return this.abort(res , 401 , logcode , "ایمیل با فرمت درست وارد شود" ) 
+                    }).test(email)) return this.abort(res , 401 , null , "ایمیل با فرمت درست وارد شود" ) 
 
-                let hashedPassword
-                if (password) hashedPassword = await this.helper.hashPassword(password);
-                else hashedPassword = "undefined"
-                const newUser = new User({
-                    name,
-                    username,
-                    contact,
-                    email,
-                    password: hashedPassword,
-                    active: false,
-                    mobile: otp.number,
-                    role: role
-                });
-                newUser.accessToken = jwt.sign({
-                    userId: newUser._id
-                }, process.env.JWT_SECRET, {
-                    expiresIn: "1d"
-                });
-                const result = await this.model.Role.findOne({role: role})
-                if (!result) new this.model.Role({role}).save()
-                
-                await newUser.save(async (err, user) => {
-                    if (err) {
-                        let message = "";
-                        if (err.errors.username) message = `${err.errors.username} `;
-                        if (err.errors.email) message += `${err.errors.email} `;
-                        if (err.errors.password) message += `${err.errors.password}`;
-                        if (err.errors.mobile) message += `${err.errors.mobile}`;
-                        message = message.trim();
-                        return this.abort(res , 401 , logcode , message ) 
-                    } else {
-                        await this.model.Otp.deleteMany({
-                            number: otp.number
-                        })
-                        return this.ok(res , 200 , logcode , "User registrations is successful.") 
-                    }
-                });
+            let hashedPassword
+            if (password) hashedPassword = await this.helper.hashPassword(password);
+            else hashedPassword = "undefined"
+            const newUser = new this.model.User({
+                name,
+                username,
+                contact,
+                email,
+                password: hashedPassword,
+                active: false,
+                mobile: mobile,
+                role: role
             });
+            newUser.accessToken = jwt.sign({
+                userId: newUser._id
+            }, process.env.JWT_SECRET, {
+                expiresIn: "1d"
+            });
+            // const result = await this.model.Role.findOne({userRef : newUser._id})
+            // if (!result) new this.model.Role({role}).save()
+            new this.model.Role({
+                role,
+                userRef : newUser._id,
+            }).save()
+
+            await newUser.save(async (err, user) => {
+                if (err) {
+                    let message = "";
+                    if (err.errors.username) message = `${err.errors.username} `;
+                    if (err.errors.email) message += `${err.errors.email} `;
+                    if (err.errors.password) message += `${err.errors.password}`;
+                    if (err.errors.mobile) message += `${err.errors.mobile}`;
+                    message = message.trim();
+                    return this.abort(res , 401 , null , message ) 
+                } else {
+                    //handle verify acc
+                    //send otp verfication
+                    this.sendOtp(newUser._id , newUser.mobile, res);
+                    
+                    // await this.model.Otp.deleteMany({
+                    //     number: otp.number
+                    // })
+
+                    // return this.ok(res , 200 , null , "User registrations is successful.") 
+                }
+            });
+        
         } catch (error) {
             next(error)
         }
     };
+
+
+    async sendOtp(_id , number , res ){
+        const OTPCode = otpGenerator.generate(6, {
+            digits: true,
+            lowerCaseAlphabets: false,
+            upperCaseAlphabets: false,
+            specialChars: false
+        })
+        const otp = new this.model.Otp({
+            number: number,
+            otp: OTPCode
+        })
+        const salt = await bcrypt.genSalt(10)
+        otp.otp = await bcrypt.hash(otp.otp, salt);
+        otp.userId = _id;
+        await otp.save()
+        console.log(OTPCode)
+        const data = JSON.stringify({
+            Code: OTPCode,
+            MobileNumber: number
+        });
+        res.json({
+            status : "Pending",
+            message : "verification code sent",
+            data : {
+                userId : _id ,
+                number : number,
+                otp : OTPCode
+            }
+        })
+    }
 
 })()
