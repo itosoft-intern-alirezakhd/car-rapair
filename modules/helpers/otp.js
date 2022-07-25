77555ff8f8cb413313b4c4b6a268196b8284f865
alireza <alirezakoohzad2705@gmail.com>
2022-07-25 22:53:02 +0430

    add send otp function to otp helper

---
+++ modules/helpers/otp.js
@@ -0,0 +1,40 @@
+import Otp from "../models/otp-model.js"
+import otpGenerator from 'otp-generator'
+import bcrypt from 'bcrypt'
+
+
+export const otpGenerate = async (number)=>{
+    const OTPCode = otpGenerator.generate(6, {
+        digits: true,
+        lowerCaseAlphabets: false,
+        upperCaseAlphabets: false,
+        specialChars: false
+    })
+    const otp = Otp({
+        number: number,
+        otp: OTPCode
+    })
+    const salt = await bcrypt.genSalt(10)
+    otp.otp = await bcrypt.hash(otp.otp, salt)
+    await otp.save()
+    console.log(OTPCode)
+    const data = {
+        code: OTPCode,
+        mobileNumber: number
+    };
+
+    return data
+}
+
+export const sendOtp = async (_id , number , res)=>{
+    const result = await otpGenerate(number);
+    res.json({
+        status : "Pending",
+        message : "verification code sent",
+        data : {
+            userId : _id ,
+            number : number,
+            otp : result.code
+        }
+    })
+}
