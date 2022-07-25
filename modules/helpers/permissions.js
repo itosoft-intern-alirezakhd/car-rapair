89ff5014db602abd8bf79d89ec11b341e650e984
alireza <alirezakoohzad2705@gmail.com>
2022-07-25 23:29:13 +0430

    adding defualt permissions for superadmin and admin

---
+++ modules/helpers/permissions.js
@@ -0,0 +1,51 @@
+export const superAdminPermissions  = [
+    {
+        action : "readAny",
+        resource : "user"
+    },
+    {
+        action : "createAny",
+        resource : "user"
+    },
+    {
+        action : "deleteAny",
+        resource : "user"
+    },
+    {
+        action : "updateAny",
+        resource : "user"
+    },
+    {
+        action : "readAny",
+        resource : "role"
+    },
+    {
+        action : "createAny",
+        resource : "role"
+    },
+    {
+        action : "deleteAny",
+        resource : "role"
+    },
+    {
+        action : "updateAny",
+        resource : "role"
+    },
+]
+
+
+
+export const basicPermissions  = [
+    {
+        action : "readAny",
+        resource : "profile"
+    },
+    {
+        action : "readAny",
+        resource : "user"
+    },
+    {
+        action : "readAny",
+        resource : "role"
+    }
+]
