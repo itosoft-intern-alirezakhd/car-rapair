6462f34b3386ea0a2edcca2127a8fee2c593e110
alireza <alirezakhd2705@gmail.com>
2022-07-27 21:15:52 +0430

    debugging check admin and superadmin

--- modules/routes/middlewares/is-admin-middleware.js
+++ modules/routes/middlewares/is-admin-middleware.js
@@ -1,12 +1,22 @@
 import User from "../../models/user-model.js";
+import Role from "../../models/role-model.js";
 
 export default async (req , res, next)=>{
     try{
-    let userLogged = res.locals.loggedIn;
+    let userLogged = res.locals.loggedInUser;
     if(!userLogged) return res.status(401).json({message : "user does not exist"})
-    let user = await User.findOne({_id : userLogged._id , role: "admin"});
-    if(!user) return res.status(401).json({message : "user does not exist"});
-    next();    
+    let user = await User.findOne({_id : userLogged._id });
+        const role = await Role.findById({
+            _id : user.role
+        })
+        if(!role){
+            return res.status(401).json({message : "token is wrong"});
+        }
+        if(role.role ==="admin"){
+            next()
+        }else{
+            return res.status(401).json({message : "you are not admin"});
+        }
     }catch(error){
         next(error);
     }
--- modules/routes/middlewares/is-superAdmin-middleware.js
+++ modules/routes/middlewares/is-superAdmin-middleware.js
@@ -4,14 +4,18 @@ import mongoose from "mongoose";
 
 export default async (req , res, next)=>{
     try{
-
     let userLogged = res.locals.loggedInUser;
     if(!userLogged) return res.status(401).json({message : "user does not exist"})
     let user = await User.findOne({_id : userLogged._id});
     const role = await Role.findById({
         _id : user.role
     })
+    if(!role){
+        return res.status(401).json({message : "token is wrong"});
+    }
+    
     if(role.role ==="superAdmin"){
+        
         next()
     }else{
         return res.status(401).json({message : "you are not superAdmin"});
