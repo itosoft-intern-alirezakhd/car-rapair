938b9f464511aaa80f2a29506681e029a3300570
alireza <alirezakhd2705@gmail.com>
2022-07-31 00:03:59 +0430

    adding  car tips controller

---
+++ modules/controllers/api/v1/shareController/car/tipController.js
@@ -0,0 +1,18 @@
+import {InitializeController} from '../../basicController/car/initializeController.js'
+
+export default new (class TypeController extends InitializeController {
+
+    async getTips(req , res , next){
+        try{
+            const  tips =await  this.model.Car.find().select('enName tips') 
+            if(!tips) return this.abort(res, 404, null, "not found tips")
+            this.helper.response(res, "get tips successfully", null, 200, tips)
+        }catch(err){
+            next(err)
+        }
+
+
+    }
+
+
+})()
