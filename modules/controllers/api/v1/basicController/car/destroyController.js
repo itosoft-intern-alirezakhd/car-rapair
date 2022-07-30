a6456fa848b0921bc701bdf8cbb53f554c7dd7b0
alireza <alirezakhd2705@gmail.com>
2022-07-30 19:08:01 +0430

    implementing delete  car router for user

--- modules/controllers/api/v1/basicController/car/destroyController.js
+++ modules/controllers/api/v1/basicController/car/destroyController.js
@@ -3,18 +3,19 @@ import { InitializeController } from "./initializeController.js";
 
 
 export default new (class DestroyController extends InitializeController{
+    
     async deleteCar (req, res)  {
         try {
             const carId = req.body.carId;
             const car = await this.model.UserCar.findOne({
-                _id: carId , userId : req.user._id
+                _id: carId
             })
-            
-            const result = await this.model.Car.findByIdAndDelete(req.body._id)
+            if(!car) return this.abort(res , 404 , null , "car not found")
+            const result = await this.model.UserCar.findByIdAndDelete(carId)
             if (result) return this.helper.response(res , "Deleted car successfully" , null ,  200 ,  result) 
-            else return this.abort(res , 401 , null , "_id Not Found!") 
+            else return this.abort(res , 404 , null , "_id Not Found!") 
         } catch (err) {
-            if (err.path === '_id') return this.abort(res , 401 , null , "_id Not Found!") 
+            if (err.path === '_id') return this.abort(res , 404 , null , "_id Not Found!") 
             return this.abort(res , 500 , null , err);
         }
     }
