9de86ea6bf57a7fe3cdeff8c465eb18ff32891bd
alireza <alirezakhd2705@gmail.com>
2022-08-05 21:56:31 +0430

    fixed createController logic

--- modules/controllers/api/v1/adminController/role/createController.js
+++ modules/controllers/api/v1/adminController/role/createController.js
@@ -1,10 +1,11 @@
 import User from "../../../../../models/user-model.js";
-import { InitializeController } from "../../superAdminController/role/initializeController.js";
+import {validationResult} from 'express-validator'
+import { InitializeController } from "./initializeController.js";
+import Role from "../../../../../models/role-model.js";
 
+export default new(class createController extends InitializeController {
 
-export default new (class createController extends InitializeController{
-
-    async addRole (req, res, next) {
+    async addRole(req, res, next) {
         try {
             const {
                 role,
@@ -12,49 +13,45 @@ export default new (class createController extends InitializeController{
                 permissions,
                 userId
             } = req.body;
-            if(userId  === req.user._id && (req.user.role  === "superAdmin" || req.user.role  === "admin" ) ) return this.abort (res , 401 , null , "you can not add role for own ")
+            //check validation
+            let errors  = validationResult(req);
+            if(!errors.isEmpty()){
+                return this.showValidationErrors(res , errors.array())
+            }
+            if (userId === req.user._id &&
+                (req.user.role === "superAdmin" || req.user.role === "admin"))
+                     return this.abort(res, 401, null, "you can not add role for own ")
+            //find user
             const user = await User.findById(userId);
-            if(!user) return this.abort(res , 404 , null , "this user does not exist ")
-            const isRoleExist = await this.model.Role.findOne({role : role })
-            if(isRoleExist){
+            if (!user) return this.abort(res, 404, null, "this user does not exist ")
+            //check role existing 
+            let isRoleExist = await Role.findOne({userRef : user._id , role : role});
+        
+            if (isRoleExist ) {
                 throw new Error('role has already exist')
-            }  
-            
-            const roles = await this.model.Role.find({userRef : userId});
-            roles.forEach(element => {
-                if(element.role == role){
-                    return this.abort(res , 401 , null , "this role with this user has already exist ")
-                }
-            });
+            }
             //add new role for user
-            const newRole = new this.model.Role({
+            const newRole = new Role({
                 role,
                 extend,
-                permissions,
-                userRef : userId
+                permissions  ,
+                userRef: userId
             });
             //update user role
-            const findRole  = user.role.find((r)=>   r === role  )
-            if(!findRole){
-                user.role.push(role) ;
+            const findRole = user.role.find((r) => r === role)
+            if (!findRole) {
+                user.role.push(role);
                 await user.save();
-            }else {
-                return this.abort(res , 401 , null , "user has this role")
+            } else {
+                return this.abort(res, 401, null, "user has this role")
             }
-            try{
-                await newRole.save()
-                this.helper.response(res , null , null ,200 , {
-                    success: true,
-                    message: 'Role Add successfully'
-                })
-                // getGrants();
-            }catch{
-                res.status(400).json(err.message)
-            }
+            //saving role
+            await newRole.save();
+            this.helper.response(res, 'Role Add successfully', null, 200,newRole);
         } catch (err) {
             next(err);
         }
     };
-   
+
 
 })()
