7a492b36f5b496cd003e9917a05ee081236a9e76
alireza <alirezakhd2705@gmail.com>
2022-08-05 21:56:20 +0430

    fixed updateController logic

--- modules/controllers/api/v1/adminController/role/updateController.js
+++ modules/controllers/api/v1/adminController/role/updateController.js
@@ -1,24 +1,37 @@
-import { InitializeController } from "../../superAdminController/role/initializeController.js";
+import User from "../../../../../models/user-model.js";
+import {
+    InitializeController
+} from "./initializeController.js";
 
 
-export default new (class updateController extends InitializeController{
+export default new(class updateController extends InitializeController {
 
-    async updateRole  (req, res, next) {
+    async updateRole(req, res, next) {
         try {
-            const {role , extend , permissions } = req.body;
-            let roleId  = req.params.roleId
+            const {
+                role,
+                extend,
+                permissions
+            } = req.body;
+            let roleId = req.params.roleId;
+            if (!roleId) return this.abort(res, 400, null, "id is undefined")
             let updateRole = {};
-            if(role) updateRole.role  = role;
-            if(extend) updateRole.extend  = extend;
-            if(permissions) updateRole.permissions  = permissions;
-            if(updateRole  === {})
-                return this.abort(res , 400 , null , "information for updating role is not enough")
-            
-            await this.model.Role.findByIdAndUpdate(roleId, updateRole)
-            this.helper.response(res, null , null , 200 ,{
-                success: true,
-                message: 'Role has been updated'
-            } )
+            if (role) updateRole.role = role;
+            if (extend) updateRole.extend = extend;
+            if (permissions) updateRole.permissions = permissions;
+            if (updateRole === {})
+                return this.abort(res, 400, null, "information for updating role is not enough")
+
+            let prevRole = await this.model.Role.findById(roleId);
+            const user = await User.findById(prevRole.userRef);
+            let roleName = prevRole.role;
+            let roleIndex = user.role.findIndex(e => e === roleName);
+            user.role[roleIndex] = role;
+            user.markModified('role');
+            await user.save()
+            await this.model.Role.findByIdAndUpdate(roleId, updateRole);
+            let result = await this.model.Role.findById(roleId);
+            this.helper.response(res, 'Role has been updated', null, 200, result)
             // getGrants();
         } catch (error) {
             next(error)
